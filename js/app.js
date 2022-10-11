1eaee05770ed436e4279a841dcf6db602d475b18
N3f73r <n3f73r@gmail.com>
2022-10-10 23:59:30 -0500

    arreglar colisiones

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+let player; 
 
 class MainScene extends Phaser.Scene {
     constructor(){
@@ -8,23 +9,69 @@ class MainScene extends Phaser.Scene {
         // Cargo el json del mapa
         this.load.image('tiles', './assets/spritesheet.png');
         this.load.tilemapTiledJSON('map', './assets/ground.json');
+        this.load.spritesheet('player', './assets/sprites/penguin.png', {frameWidth: 72, frameHeight: 64});
     }
  
     create(){
         // Creo el tilemap y asigno llave y dimensiones
-        const map = this.make.tilemap({ key: "map", tileWidth: 128, tileHeight: 128});
+            const map = this.make.tilemap({ key: "map", tileWidth: 128, tileHeight: 128});
         // Añado el tileset (las imagenes). Aquí se necesito el nombre del tileset en Tiled y la llave del preload
-        const tileset = map.addTilesetImage("tiles1","tiles");
+            const tileset = map.addTilesetImage("tiles1","tiles");
         // Añado las capas. Utilizo el nombre de la capa en Tiled, el tileset que acabo de añadir, y las coordenadas x & y
-        const layer2 = map.createLayer("sky", tileset, 0, 0);
-        const layer1 = map.createLayer("water", tileset, 0, 0);
-        const layer = map.createLayer("ground", tileset, 0, 0);
+            const layer2 = map.createLayer("sky", tileset, 0, 0);
+            const layer1 = map.createLayer("water", tileset, 0, 0);
+            const layer = map.createLayer("ground", tileset, 0, 0);
+        // NOTA: El mapa de Tield debe tener el calculo de bloques para tener el mismo tamaño del proyecto.
 
-        // El mapa de Tield debe tener el calculo de bloques para tener el mismo tamaño del proyecto.
+        /* Player */
+            player = this.physics.add.sprite(384, 200, 'player');
+        /* --- */
 
+
+        /* Animations */
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', {start: 3, end: 6}),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNumbers('player', {start: 0, end: 2}),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'iddle',
+            frames: this.anims.generateFrameNumbers('player', {start: 3, end: 3}),
+            frameRate: 8,
+            repeat: -1
+        });
+        /* --- */
+
+        /* Colitions */
+            // Para la colicion defino primero la colsion con la capa entera y luego especifico el Id del objeto
+            this.physics.add.collider(player, layer);
+            layer.setCollisionBetween(0, 3);
+        /* --- */
     }
 
     update(){
+        let scanner = this.input.keyboard.createCursorKeys();
+        let velocityX = 160;
+        if (scanner.left.isDown){
+            player.setVelocityX(velocityX);
+            player.anims.play('right', true);
+            player.flipX = true;
+        }else if (scanner.right.isDown){
+            player.setVelocityX(-velocityX);
+            player.anims.play('right', true);
+        }else {
+            player.setVelocityX(0);
+            player.anims.play('iddle', true);
+        }
     }
 }
 
